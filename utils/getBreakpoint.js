0c07fd24e5a5b67c23b35a1a6491f269aaa71d21
Guido Torres <guido732@gmail.com>
2020-07-05 16:00:55 -0300

    feature (getBreakpoint util) creates utils folder, adds getBreakpoint util

---
+++ utils/getBreakpoint.js
@@ -0,0 +1,29 @@
+// Hooks
+import { useWindowSize } from "custom_hooks/useWindowSize";
+// Theme
+import { breakpoints } from "styles/_breakpoints";
+
+const getBreakpoint = () => {
+	let size;
+	const { width } = useWindowSize();
+	switch (true) {
+		case width <= parseInt(breakpoints.sm):
+			size = "xs";
+			break;
+		case width <= parseInt(breakpoints.md):
+			size = "sm";
+			break;
+		case width <= parseInt(breakpoints.lg):
+			size = "md";
+			break;
+		case width <= parseInt(breakpoints.xl):
+			size = "lg";
+			break;
+		default:
+			size = "xl";
+			break;
+	}
+	return size;
+};
+
+export { getBreakpoint };
