96faa9ba899e69a0144cf1618900b2d433105692
Guido Torres <guido732@gmail.com>
2020-07-05 15:22:51 -0300

    feature (useWindowSize hook) adds hook that listens to screen size changes

--- styles/utilities.jsx
+++ styles/utilities.jsx
@@ -1,7 +1,8 @@
 import { css } from "styled-components";
+import { theme } from "./theme";
 
 export const CenteredContent = css`
 	margin: auto;
-	max-width: 1170px;
+	max-width: ${theme.breakpoints.lg};
 	width: 100%;
 `;
