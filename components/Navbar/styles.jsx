8fc517811e71d3a726597988beeba830239fe534
Guido Torres <guido732@gmail.com>
2020-07-05 13:41:29 -0300

    feature (navbar) adds markup and creates style files

---
+++ components/Navbar/styles.jsx
@@ -0,0 +1,20 @@
+// Styled Comps
+import styled, { css } from "styled-components";
+// Next
+import Link from "next/link";
+// Utils
+import { CenteredContent } from "styles/utilities";
+
+export const StyledNavbar = styled.nav`
+	/* ${CenteredContent} */
+	display: flex;
+	justify-content: space-between;
+`;
+
+export const BrandContainer = styled.div`
+	display: block;
+`;
+
+export const StyledLink = styled(Link)`
+	color: inherit;
+`;
