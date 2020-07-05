b1987272abf8c200cc1c950ed8f239ae6f4cafb3
Guido Torres <guido732@gmail.com>
2020-07-05 11:56:33 -0300

    feature (color constants + footer) Adds base files + structure for footer

---
+++ components/Footer/index.jsx
@@ -0,0 +1,22 @@
+// React
+import React from "react";
+// Styles
+import { StyledFooter, Separator, MediaContainer, Media } from "./styles";
+
+const Footer = () => (
+	<StyledFooter>
+		<div>{/* Main footer content */}</div>
+		<Separator horizontal />
+		<div>
+			<p>Diseño por GTD - 2020. Todos los derechos reservados</p>
+			<MediaContainer>
+				<Media>1</Media>
+				<Media>2</Media>
+				<Media>3</Media>
+				<Media>4</Media>
+			</MediaContainer>
+		</div>
+	</StyledFooter>
+);
+
+export default Footer;
---
+++ constants/colors.js
@@ -0,0 +1,8 @@
+export default {
+	main: "#fff",
+	secondary: "#f1f1f1",
+	highlight: "#e53247",
+	textGray: "#808080",
+	textDark: "#333131",
+	textDarkest: "#282727",
+};
