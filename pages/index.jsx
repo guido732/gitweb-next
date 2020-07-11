317bee53e98d20ff65414820d3a09c5b2245e380
Guido Torres <guido732@gmail.com>
2020-07-11 20:21:04 -0300

    feature (Component mount architecture) Moves Navbar, Footer and Layout components to top level _app.jsx to avoid unmounting on page changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,21 +12,7 @@ export default function Home() {
 				<title>Guido Torres - Frontend Web Dev</title>
 				<link rel="icon" href="/img/favicon/favicon.ico" />
 			</Head>
-			<Navbar />
-			<Layout>
-				<Header />
-			</Layout>
-
-			<footer>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					footy
-					{/* Powered by <img src="/img/logo.svg" alt="GTD Logo" className="logo" /> */}
-				</a>
-			</footer>
+			<Header />
 		</>
 	);
 }
