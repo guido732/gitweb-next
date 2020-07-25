26113f0051b98148e1db89f6dc19f709a8b0d93e
Guido Torres <guido732@gmail.com>
2020-07-25 19:13:14 -0300

    feature (About) Adds layout for site and conditional rendering of components based on radio buttons

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+// React
+import React, { useState } from "react";
+// Custom Components
+import Layout from "components/Layout";
+import CallToAction from "components/CallToAction";
+
+const DummyComponent1 = () => <div>Conocimiento</div>;
+const DummyComponent2 = () => <div>Experiencia</div>;
+const DummyComponent3 = () => <div>Formación</div>;
 
 const About = () => {
-	return <div>About page</div>;
+	const [selectedTab, setSelectedTab] = useState("knowledge");
+
+	const getTabContent = () => {
+		switch (selectedTab) {
+			case "knowledge":
+				return <DummyComponent1 />;
+			case "experience":
+				return <DummyComponent2 />;
+			case "formation":
+				return <DummyComponent3 />;
+			default:
+				return <DummyComponent1 />;
+		}
+	};
+
+	const setTabcontent = (e) => {
+		const { id } = e.target;
+		setSelectedTab(id);
+	};
+
+	return (
+		<>
+			<Layout>
+				<header>
+					<div>
+						<img src="" alt="imagen" />
+					</div>
+					<div>
+						<h2>texto</h2>
+						<div>descripcion</div>
+					</div>
+				</header>
+				<section>
+					<fieldset>
+						{/* <legend>Elige un color</legend> */}
+						<label>
+							<input
+								type="radio"
+								name="color"
+								value="azul"
+								id="knowledge"
+								onChange={setTabcontent}
+								checked={selectedTab === "knowledge"}
+							/>
+							Conocimiento
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="color"
+								value="negro"
+								id="experience"
+								onChange={setTabcontent}
+								checked={selectedTab === "experience"}
+							/>
+							Experiencia
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="color"
+								value="amarillo"
+								id="formation"
+								onChange={setTabcontent}
+								checked={selectedTab === "formation"}
+							/>
+							Formación
+						</label>
+					</fieldset>
+					{getTabContent()}
+				</section>
+			</Layout>
+			<CallToAction />
+		</>
+	);
 };
 
 export default About;
