200bc65fe078b49a1f4845a4fd7a5ac399d9ce3e
Guido Torres <guido732@gmail.com>
2020-08-04 20:20:38 -0300

    chore: regen site

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8380c241215f3181afc2";
+/******/ 	var hotCurrentHash = "87780397310a92021032";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
